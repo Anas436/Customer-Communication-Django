ca6f95d977c0b2c0803bb931f1274a9d850d48f7
spear97 <72108425+spear97@users.noreply.github.com>
2024-02-22 09:30:17 -0600

    Update manage.py

--- Customer360/manage.py
+++ Customer360/manage.py
@@ -6,15 +6,18 @@ import sys
 
 def main():
     """Run administrative tasks."""
+    # Set the Django settings module for the command-line utility
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'customer360.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
+        # Handle import error if Django is not installed
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    # Execute Django commands from the command line
     execute_from_command_line(sys.argv)
 
 
