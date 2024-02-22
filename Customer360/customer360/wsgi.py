ccc5e7937e8305e3bf9a7fb62aef698996b2d848
spear97 <72108425+spear97@users.noreply.github.com>
2024-02-22 09:29:43 -0600

    Update wsgi.py

--- Customer360/customer360/wsgi.py
+++ Customer360/customer360/wsgi.py
@@ -1,4 +1,4 @@
-"""
+""""
 WSGI config for customer360 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+# Set the Django settings module for the WSGI application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'customer360.settings')
 
+# Get the WSGI application for this project
 application = get_wsgi_application()
